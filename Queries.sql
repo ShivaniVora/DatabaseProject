8044b7cf1b7132bb2a9121f4d803a2340c9c32fc
ShivaniVora <shivanivora797@icloud.com>
2023-07-17 12:48:18 +0200

    updated sample data, create table statements, and queries

--- Queries.sql
+++ Queries.sql
@@ -1,72 +1,104 @@
-USE journal;
-
-SET @user_field := "bobby";
-SET @pass := "password456";
-SET @firstname := "test";
-SET @lastname := "lasttest";
-SET @email := "test email2";
-SET @isUser := 1;
-SET @membershipStartDate := '2020-01-01';
-SET @isPublic := 1;
-SET @isBanned := 0;
+USE travel_journal;
 
 -- Q1 - return 0 if admin, 1 if user, empty table if user Does Not Exist
-SELECT IsUser FROM account WHERE username = @user_field AND UserPassword = @pass;
+-- user and pass are passed in values from the user input on screen
+
+SELECT IsUser FROM ACCOUNT WHERE username = "user" AND UserPassword = "pass";
 
 -- Q2 - throws error if account fails
-INSERT INTO account VALUES
-	(@firstname, @lastname, @user_field,@email,@pass,@isUser,@membershipStartDate,@isPublic,@isBanned);
+-- all passed in values of first, last, user, email, pass, and isUser
+-- start date is current date, isBanned starts at FALSE by default
+INSERT INTO ACCOUNT (FirstName, LastName, Username, Email, UserPassword, IsUser, MembershipStartDate, isPublic)
+	VALUES ("first", "last", "userName", "email@email", "pass", 1, CURDATE(), 0);
 
 -- Q3 NOT A QUERY
 
-
--- Q5
-DELETE FROM account WHERE username = @user_field;
-
 -- Q4 - return empty table if not admin/no user exists, a row with values otherwise
-SELECT * FROM account WHERE username = @user_field AND IsUser = 1;
+-- "user" is information stored from the previous screen
 
+SELECT FirstName, LastName, Email, UserPassword, IsPublic
+FROM ACCOUNT WHERE Username = "user" AND IsUser = 1;
 
--- Q6
-UPDATE account
-SET FirstName = "Change me", LastName = "change me next", Email = "change me email", UserPassword = "Change me pwd", IsPublic = "0"
-WHERE Username=@user_field;
+-- Q5 deletes the account, "user" is stored from previous screen
+DELETE FROM ACCOUNT WHERE Username = "userName";
 
-SELECT * FROM account;
+-- Q6 updates an account, "user" is stored from previous screen
 
+UPDATE ACCOUNT
+SET FirstName = "NewFirst", LastName = "NewLast", Email = "NewEmail", UserPassword = "NewPass", IsPublic = "0"
+WHERE Username="userEdit";
 
+/* Query 7 - SQL is used to store a new trip definition. (Or reject it if the trip name is a
+duplicate.) 
+all information is input except for username which is passed from previous screens */
+
+INSERT INTO TRIP (TripName, Username, StartDate, EndDate)
+VALUES ('Summer in Barcelona', 'user', '2023-05-12', '2023-07-29');
+
+/* Query 8 - Table is automatically populated with all cities and their avg rating based on
+all users ratings (as long as the rating is public). */
+
+SELECT CITY.CityName, CITY.Country, AVG(JOURNAL_ENTRY.Rating)
+FROM CITY NATURAL JOIN JOURNAL_ENTRY 
+WHERE JOURNAL_ENTRY.PrivacyLevel = TRUE
+GROUP BY CITY.CityName, CITY.Country;
+
+/* Query 9 - “Search for City” textbox filters down to just “London” for example if you
+that is typed in and “Search” button is clicked (updating this window). */
+
+SELECT CITY.CityName, CITY.Country, AVG(JOURNAL_ENTRY.Rating)
+FROM CITY NATURAL JOIN JOURNAL_ENTRY 
+WHERE JOURNAL_ENTRY.PrivacyLevel = TRUE AND CityName = "London"
+GROUP BY CITY.CityName, CITY.Country;
+
+/* Query 10 - “Reset” goes back to all cities being loaded. (Clears the search textbox
+too.) SQL #8 and SQL #10 might be the same - you decide. */
+
+SELECT CITY.CityName, CITY.Country, AVG(JOURNAL_ENTRY.Rating)
+FROM CITY NATURAL JOIN JOURNAL_ENTRY 
+WHERE JOURNAL_ENTRY.PrivacyLevel = TRUE
+GROUP BY CITY.CityName, CITY.Country;
 
 -- SQL #11: this window is populated with all the (public) entries from all users about one given city.
--- Dummy data: city/JE location ID
+-- Dummy data: City Name and Country Name from the previous screen 
+
 SELECT EntryDate, Rating, Note
 FROM JOURNAL_ENTRY
 WHERE CityName = "Dublin" AND Country = "Ireland" AND PrivacyLevel = True;
 
 -- SQL #12: This is populated by SQL about a single public city journal entry from any user.
--- Dummy data: from previous screen city/JE location ID, EntryID
+-- Dummy data: from previous screen EntryID 
+
 SELECT CityName, EntryDate, Rating, Note
-FROM (JOURNAL_ENTRY NATURAL JOIN CITY)
+FROM JOURNAL_ENTRY
 WHERE EntryID = 2;
 
 -- SQL #13: Pressing “report” will flag the entry, save the info about who flagged the entry, and save each of the reasons
-INSERT INTO USER_FLAGS(Username, EntryID)
-VALUES ("camloyet", 2);
-INSERT IGNORE INTO FLAG_REASON
-VALUES (2, "Harassment");
+-- user and the entry id are passed from the previous screen, flags from current screen input
 
+INSERT INTO USER_FLAGS
+VALUES ("user", 2);
+
+INSERT INTO FLAG_REASON
+	VALUES ("user", 2, "Harrassment"), ("user", 2, "Explicit Language");
+
+/*
 -- SQL #14: Public vs Private. The user can choose to click to change that if they want to
-SELECT IsPublic
+-- preset comes from account settings (0 if private, 1 if public) */
+
+SELECT IsPublic 
 FROM ACCOUNT
-WHERE Username = "camloyet";
+WHERE Username = "user";
 
 -- SQL #15 to save the city journal entry and then return the user to the home screen
+-- user, isPublic come from the stored values, rest come from input
 
 INSERT INTO JOURNAL_ENTRY(Username,EntryDate, Note, Rating, PrivacyLevel, CityName, Country) 
-VALUES ("camloyet", "2022-08-20", "Great visit!", 5, true, "Dublin", "Ireland");
-
+VALUES ("user", CURDATE(), "Great visit!", 5, True, "Dublin", "Ireland");
 
 -- SQL #16: query populates this screen with the user’s trip names. Clicking a trip name takes the user to the “My Trip Report” screen.
 -- "user" is the username of the currently logged in user, has to be passed from previous screens
+
 SELECT TripName
 FROM TRIP
 WHERE Username = "user";
@@ -74,6 +106,7 @@ WHERE Username = "user";
 -- SQL #17: query populates this screen with the user’s trip names. Clicking a trip name takes the user to the “My Trip Report” screen.
 -- "user" is the username of the currently logged in user, has to be passed from previous screens
 -- "trip" is the trip name of the trip that the user clicked on, has to be passed from previous screens
+
 SELECT EntryDate, CityName, Country, Rating, Note
 FROM JOURNAL_ENTRY NATURAL JOIN CITY
 WHERE Username = "user" AND EntryDate BETWEEN (SELECT StartDate
@@ -82,19 +115,24 @@ WHERE Username = "user" AND EntryDate BETWEEN (SELECT StartDate
                                                 AND
                                                 (SELECT EndDate
 												FROM TRIP
-												WHERE TripName = "trip" AND Username = "user");
+												WHERE TripName = "trip" AND Username = "user")
+ORDER BY EntryDate;
 
 -- SQL #18: “Or show all of my entries” shows all of this user’s journal entries. (public and private
 -- alike whether or not they are part of any trip range of dates)
--- "user" is the username of the currently logged in user, has to be passed from previous screens
+-- "user" is the username of the currently logged in user, has to be passed from previous screens 
+
 SELECT EntryDate, CityName, Country, Rating, Note
 FROM JOURNAL_ENTRY NATURAL JOIN CITY
-WHERE Username = "user";
+WHERE Username = "user"
+ORDER BY EntryDate;
 
 -- SQL #19: Clicking on “City” reorders the info alphabetically by city name (A-Z)
 -- alike whether or not they are part of any trip range of dates)
 -- "user" is the username of the currently logged in user, has to be passed from previous screens
 -- "trip" is the trip name of the trip that the user clicked on, has to be passed from previous screens
+-- in the java, whether or not all cities are being shown should be determined
+
 SELECT EntryDate, CityName, Country, Rating, Note
 FROM JOURNAL_ENTRY NATURAL JOIN CITY
 WHERE Username = "user"
@@ -114,39 +152,41 @@ ORDER BY CityName ASC;
 -- We get here from My Trip Report having clicked on a Note in the list.
 -- SQL #20: populates this screen.
 -- entryId comes from the past screen based on which note the user clicks on and which entry that is from
+
 SELECT EntryDate, CityName, Rating, Note
-FROM JOURNAL_ENTRY NATURAL JOIN CITY
-WHERE EntryID = 1;
+FROM JOURNAL_ENTRY
+WHERE EntryID = 2;
+
 
 -- BUT, “delete” deletes this city entry via SQL #21.
 -- entryId comes from the past screen based on which note the user clicks on and which entry that is from
 DELETE FROM JOURNAL_ENTRY
-WHERE EntryID = 1;
+WHERE EntryID = 2;
 
 -- #22: SQL query used to help auto-populate this screen,
 -- and each Note in the list is clickable to take us to the Admin’s Review City Entry screen.
 
-USE journal;
-
 SELECT
-	je.username,
-    je.cityName, 
+	je.Username,
+    je.CityName, 
     je.Note,
-    GROUP_CONCAT(fr.Reason),
+    GROUP_CONCAT(fr.Reason SEPARATOR ", "),
     uf.USERNAME AS FLAGGER
 FROM
     JOURNAL_ENTRY je
     JOIN USER_FLAGS uf ON je.EntryID = uf.EntryID
-    JOIN FLAG_REASON fr ON uf.FlagID = fr.FlagID
+    LEFT OUTER JOIN FLAG_REASON fr ON uf.Username = fr.Username AND uf.EntryID and fr.EntryID
 GROUP BY
     je.Username,
     je.CityName,
     je.Note,
     uf.Username;
-    
+
 -- #23: not a query
 
 -- #24: populates the "Review Flagged Entry (admin)" 
+-- entry id passed in from the clicked on flag in the past screen
+
 SELECT
 	je.cityName,
     je.EntryDate,
@@ -154,28 +194,31 @@ SELECT
     je.Note
 FROM
 	JOURNAL_ENTRY je
-    JOIN USER_FLAGS uf ON je.EntryID = uf.EntryID
 WHERE
-    uf.FlagID IS NOT NULL;
-    
+	EntryID = 4;
+ 
 -- #25: "Clear Flag" button that clears the flag from the entry
-UPDATE USER_FLAGS
-SET FlagID = null 
-WHERE EntryID = 1;
+-- will remove all flags related to the entry, entryID passed from previous screen
+
+DELETE FROM USER_FLAGS
+WHERE EntryID = 5;
 
 -- #26: "Delete entry" button which deletes this one entry
+-- entryId passed from previous screen
+
 DELETE FROM JOURNAL_ENTRY
-WHERE EntryID = 1;
+WHERE EntryID = 3;
 
 -- #27: "Ban User" marks the user as banned, and all of their entries are deleted.
 -- mark the user as banned
-UPDATE USER
+
+UPDATE ACCOUNT
 SET IsBanned = TRUE
-WHERE username = "camloyet";
+WHERE Username = "user";
 
 -- delete all entries of the banned user
 DELETE FROM JOURNAL_ENTRY
-WHERE Username = "camloyet";
+WHERE Username = "user";
 
 
 
--- TJSampleData.sql
+++ TJSampleData.sql
@@ -4,37 +4,48 @@ INSERT INTO ACCOUNT
 VALUES
     ("Cameron", "Loyet", "camloyet", "cameron@gmail.com", "password1234", True, "2020-08-08", FALSE, FALSE),
 	("Bob", "Bober", "bobby", "bobBryan@gmail.com", "password456", False, "2013-07-21", NULL, NULL),
-    ("Tom", "Itschner", "travel_lover123", "tom@gmail.com", "travelingisfun", True, "2019-07-10", TRUE, TRUE);
-
+    ("Tom", "Itschner", "travel_lover123", "tom@gmail.com", "travelingisfun", True, "2019-07-10", TRUE, FALSE),
+    ("first", "last", "user", "email", "pass", True, "2023-07-04", True, FALSE),
+    ("first", "last", "userEdit", "emailEdit", "pass", True, "2023-07-04", True, FALSE);
 
 INSERT INTO CITY
 VALUES 
-    ("London", "United Kingdom", 1),
-    ("Dublin", "Ireland", 2),
-    ("Sydney", "Australia", 5)
-;
-
-INSERT INTO SITE (SiteName, CityName, Country, LocationID)
-VALUES 
-    ("Buckingham Palace", "London", "United Kingdom", 4),
-    ("Cliffs of Moher", "Dublin", "Ireland",  5),
-    ("Sydney Opera House", "Sydney", "Australia", 6)
-;
+	("London", "United Kingdom"),
+	("Dublin", "Ireland"),
+	("Sydney", "Australia"),
+    ("Dublin", "NotIreland");
 
 INSERT INTO TRIP
 VALUES 
     ("Trip to London!", "camloyet", "2023-06-21", "2023-06-23"),
-    ("Vacation in Dublin", "travel_lover123", "2022-06-20", "2022-06-27")
+    ("Vacation in Dublin", "travel_lover123", "2022-06-20", "2022-06-27"),
+    ("trip", "user", "2023-04-01", "2023-07-04")
 ;
 
-INSERT INTO JOURNAL_ENTRY(Username,EntryDate, LocationID, Note, Rating, PrivacyLevel)
+INSERT INTO JOURNAL_ENTRY(Username, EntryDate, Note, Rating, PrivacyLevel, CityName, Country)
 VALUES 
-    ("camloyet", "2023-07-01", 1, "Saw Buckingham Palace with friends today! Didn't see my favorite King Charles though :(",
-    4, TRUE),
-    ("travel_lover123", "2023-06-01", 2, "Loved me a good ole Irish pub!",
-    NULL, NULL),
-    ("camloyet", "2023-06-01", 6, NULL,
-    3, NULL)
+    ("camloyet", "2023-07-01", "Saw Buckingham Palace with friends today! Didn't see my favorite King Charles though :(",
+    4, TRUE, "London", "United Kingdom"),
+    ("travel_lover123", "2023-06-01", "Loved me a good ole Irish pub!",
+     NULL, NULL, "Dublin", "Ireland"),
+     ("travel_lover123", "2023-06-03", "Shamrocks rock!",
+     4, NULL, "Dublin", "Ireland"),
+     ("camloyet", "2023-06-01", NULL,
+     3, NULL, "Dublin", "Ireland"), 
+     ("travel_lover123", "2023-02-01", ":)",
+	5, TRUE, "London", "United Kingdom"),
+    ("user", "2023-04-01", "london!",
+    1, TRUE, "London", "United Kingdom"),
+    ("travel_lover123", "2023-05-01", NULL,
+    2, TRUE, "Dublin", "NotIreland"), 
+    ("user", "2023-07-01", "still in london",
+	5, TRUE, "London", "United Kingdom"), 
+    ("user", "2023-05-01", "london again",
+	1, TRUE, "London", "United Kingdom"), 
+    ("user", "2023-02-01", "dublin but ireland",
+	3, TRUE, "Dublin", "Ireland"), 
+    ("user", "2023-03-01", "dublin but not ireland",
+	4, TRUE, "Dublin", "NotIreland")
 ;
 
 INSERT INTO ENTRY_IN_TRIP
@@ -45,28 +56,13 @@ VALUES
 
 INSERT INTO REASON
 VALUES 
-    ("Profanity"),
-    ("Inappropriate Language"),
-    ("Spreads misinformation")
+    ("Harrassment"),
+    ("Explicit Language"),
+    ("Off Topic")
 ;
 
-INSERT INTO USER_FLAGS(Username, EntryID)
-VALUES 
-    ("camloyet", 1)
-;
+INSERT INTO USER_FLAGS
+VALUES ("camloyet", 5), ("travel_lover123", 3);
 
 INSERT INTO FLAG_REASON
-VALUES 
-    (1, "Profanity")
-;
-
-INSERT INTO CATEGORY
-VALUES 
-    ("Museum"),
-    ("Restaurant"),
-    ("Park");
-   
-INSERT INTO SITE_CATEGORIES
-VALUES
-    (1, "Museum"),
-    (1, "Park");
+	VALUES ("camloyet", 5, "Harrassment"), ("camloyet", 5, "Off Topic");
