7698ba5be1446afdb03af61cf95468f55d374ef0
ShivaniVora <shivanivora797@icloud.com>
2023-07-19 21:51:45 +0200

    first 4 slides gui complete

--- traveljournal/src/main/java/com/example/traveljournal/DataBaseConnector.java
+++ traveljournal/src/main/java/com/example/traveljournal/DataBaseConnector.java
@@ -10,7 +10,7 @@ public class DataBaseConnector {
         String databaseName = "travel_journal";
         String databaseUser = "root";
         //TODO put in password
-        String databasePassword = "pass";
+        String databasePassword = "shivu797";
         String url = "jdbc:mysql://localhost:3306/" + databaseName;
 
         try{
--- traveljournal/src/main/java/com/example/traveljournal/LoginController.java
+++ traveljournal/src/main/java/com/example/traveljournal/LoginController.java
@@ -26,42 +26,53 @@ public class LoginController {
 
 
     public void loginAction (ActionEvent event) throws SQLException {
-            DataBaseConnector connection = new DataBaseConnector();
-            Connection connectDB = connection.getConnection();
-            String connectQuery = "SELECT IsUser FROM ACCOUNT WHERE Username = '" + user.getText() + "' AND UserPassword = '" + pass.getText() + "';";
+        DataBaseConnector connection = new DataBaseConnector();
+        Connection connectDB = connection.getConnection();
+        String connectQuery = "SELECT IsUser FROM ACCOUNT WHERE Username = '" + user.getText() + "' AND UserPassword = '" + pass.getText() + "';";
 
-            currUN = user.getText();
-            currPass = pass.getText();
+        currUN = user.getText();
 
-            try {
-                Statement statement = connectDB.createStatement();
-                ResultSet queryOutput = statement.executeQuery((connectQuery));
-                while (queryOutput.next()) {
-                    FXMLLoader fxmlLoader = null;
+        try {
+            Statement statement = connectDB.createStatement();
+            ResultSet queryOutput = statement.executeQuery((connectQuery));
+            while (queryOutput.next()) {
+                FXMLLoader fxmlLoader = null;
 
-                    if (queryOutput.getString(1).equals("1")) {
-                        fxmlLoader = new FXMLLoader((TJApp.class.getResource("Home.fxml")));
-                        Scene scene = new Scene(fxmlLoader.load());
-                        //Stage stage = getCurrentStage(event);
-                        Stage stage = (Stage) ((Node) (event.getSource())).getScene().getWindow();
-                        stage.setScene(scene);
-                        HomeController controller = fxmlLoader.getController();
-                        controller.setInfo(currUN, currPass);
-                    } else if (queryOutput.getString(1).equals("0")) {
-                        fxmlLoader = new FXMLLoader((TJApp.class.getResource("Admin_Flags_Home_Page.fxml")));
-                        Scene scene = new Scene(fxmlLoader.load());
-                        //Stage stage = getCurrentStage(event);
-                        Stage stage = (Stage) ((Node) (event.getSource())).getScene().getWindow();
-                        stage.setScene(scene);
-                    }
+                if (queryOutput.getString(1).equals("1")) {
+                    fxmlLoader = new FXMLLoader((TJApp.class.getResource("Home.fxml")));
+                    Scene scene = new Scene(fxmlLoader.load());
+                    //Stage stage = getCurrentStage(event);
+                    Stage stage = (Stage) ((Node) (event.getSource())).getScene().getWindow();
+                    stage.setScene(scene);
+                    HomeController controller = fxmlLoader.getController();
+                    controller.setInfo(currUN);
+                } else if (queryOutput.getString(1).equals("0")) {
+                    fxmlLoader = new FXMLLoader((TJApp.class.getResource("Admin_Flags_Home_Page.fxml")));
+                    Scene scene = new Scene(fxmlLoader.load());
+                    //Stage stage = getCurrentStage(event);
+                    Stage stage = (Stage) ((Node) (event.getSource())).getScene().getWindow();
+                    stage.setScene(scene);
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-            connectDB.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        connectDB.close();
+    }
 
+    public void createAccButton (ActionEvent event) throws SQLException {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader((TJApp.class.getResource("Create Account.fxml")));
+            Scene scene = new Scene(fxmlLoader.load());
+            //Stage stage = getCurrentStage(event);
+            Stage stage = (Stage) ((Node) (event.getSource())).getScene().getWindow();
+            stage.setScene(scene);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
+}
 
 
 
