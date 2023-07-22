2381ce3118c6b5e4853afe7bab4c6655e26ec107
bryanpeavey <bryanpeavey03@gmail.com>
2023-07-22 22:44:06 +0200

    SearchCityController updates
    all functionality except being able to click on a city to load journal entries for that city

--- traveljournal/src/main/java/com/example/traveljournal/SearchCityController.java
+++ traveljournal/src/main/java/com/example/traveljournal/SearchCityController.java
@@ -1,17 +1,36 @@
 package com.example.traveljournal;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SearchCityController extends Application {
 
     public String user;
+    ObservableList<ObservableList> data;
+
+    @FXML
+    public TableView tableView;
+    @FXML
+    public TextField search;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -37,5 +56,79 @@ public class SearchCityController extends Application {
 
     public void setInfo(String user) {
         this.user = user;
+        build();
+    }
+
+    public void build() {
+        data = FXCollections.observableArrayList();
+        DataBaseConnector connection = new DataBaseConnector();
+        Connection connectDB = connection.getConnection();
+
+        String tableQuery = "SELECT CityName AS 'City', Country, AVG(Rating) AS 'Average Rating' FROM JOURNAL_ENTRY WHERE PrivacyLevel = TRUE GROUP BY CityName, Country;";
+        try {
+            Statement statement = connectDB.createStatement();
+            ResultSet result = statement.executeQuery((tableQuery));
+            tableView.getItems().clear();
+            for (int i = 0; i < result.getMetaData().getColumnCount(); i++) {
+                final int j = i;
+                TableColumn col = new TableColumn(result.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param ->
+                        new SimpleStringProperty(param.getValue().get(j).toString()));
+                tableView.getColumns().addAll(col);
+            }
+
+            while (result.next()) {
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= result.getMetaData().getColumnCount(); i++) {
+                    row.add(result.getString(i));
+                }
+                data.add(row);
+            }
+            tableView.setItems(data);
+            connectDB.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void searchCity (ActionEvent event) throws SQLException {
+        DataBaseConnector connection = new DataBaseConnector();
+        Connection connectDB = connection.getConnection();
+        String tableQuery = "SELECT CityName AS 'City', Country, AVG(Rating) AS 'Average Rating' FROM JOURNAL_ENTRY WHERE PrivacyLevel = TRUE AND CityName = '" + search.getText() + "'GROUP BY CityName, Country;";
+        try {
+            Statement statement = connectDB.createStatement();
+            ResultSet result = statement.executeQuery((tableQuery));
+            tableView.getItems().clear();
+            for (int i = 0; i < result.getMetaData().getColumnCount(); i++) {
+                final int j = i;
+                TableColumn col = new TableColumn(result.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param ->
+                        new SimpleStringProperty(param.getValue().get(j).toString()));
+                tableView.getColumns().addAll(col);
+            }
+
+            while (result.next()) {
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= result.getMetaData().getColumnCount(); i++) {
+                    row.add(result.getString(i));
+                }
+                data.add(row);
+            }
+            tableView.setItems(data);
+            connectDB.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void reset (ActionEvent event) {
+        try {
+            search.clear();
+            build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
